51a078b5098ced6beb89c534093154c2a1d26ab0
Makc Pokrovsliy <makcumka9111@yandex.ru>
2023-11-22 10:12:02 +0300

    created the site structure admin-panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
+//Pages
+import {Home} from "./pages/home/Home.tsx";
+import {Users} from "./pages/users/Users.tsx";
+import {Products} from "./pages/products/Products.tsx";
+import {Login} from "./pages/login/Login.tsx";
+
+//Components
+import {Navbar} from "./Companents/Navbar/Navbar.tsx";
+import {Footer} from "./Companents/footer/Footer.tsx";
+import {Menu} from "./Companents/menu/Menu.tsx";
+
+//React-Router
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+} from "react-router-dom";
+
+//style
+import "./styles/global.scss";
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+    const Layout = () => {
+        return (
+            <div className="main">
+                <Navbar/>
+                <div className="container">
+                    <div className="menu-container">
+                        <Menu/>
+                    </div>
+                    <div className="content-container">
+                        <Outlet/>
+                    </div>
+                </div>
+                <Footer/>
+            </div>
+        )
+    }
+
+    const router = createBrowserRouter([
+        {
+            path:"/",
+            element:<Layout/>,
+            children:[
+                {
+                  path:"/",
+                  element:<Home/>
+                },
+                {
+                  path:"users",
+                  element:<Users/>
+                },
+                {
+                  path:"products",
+                  element:<Products/>
+                },
+            ],
+        },
+        {
+            path:"/login",
+            element:<Login/>
+        }
+    ]);
+
+    return (
+        <RouterProvider router={router}/>
+    )
 }
 
 export default App
---
+++ src/Companents/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import './navbar.scss';
+
+export const Navbar = () => {
+    return (
+        <div className='navbar'>NAVBAR</div>
+    )
+}
