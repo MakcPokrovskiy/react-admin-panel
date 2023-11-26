beeb8ab79051ade9f7c8d43324e59f1a08bb2c8a
Makc Pokrovsliy <makcumka9111@yandex.ru>
2023-11-26 12:18:29 +0300

    complete HomePage admin-panel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,20 @@ import './home.scss';
 //Components
 import {HomeTopBox} from "../../Companents/HomeTopBox/HomeTopBox.tsx";
 import {ChartBox} from "../../Companents/ChartBox/ChartBox.tsx";
-import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from "../../data.ts";
+import {BarChartBox} from "../../Companents/BarChartBox/BarChartBox.tsx";
+import {PieChartBox} from "../../Companents/PieChartBox/PieChartBox.tsx";
+import {BigChartBox} from "../../Companents/BigChartBox/BigChartBox.tsx";
+
+//charts
+import {
+    barChartBoxRevenue,
+    barChartBoxVisit,
+    chartBoxConversion,
+    chartBoxProduct,
+    chartBoxRevenue,
+    chartBoxUser
+} from "../../data.ts";
+
 
 export const Home = () => {
     return (
@@ -11,16 +24,14 @@ export const Home = () => {
             <div className="box box1">
                 <HomeTopBox/>
             </div>
-            <div className="box box2">
-                <ChartBox {...chartBoxUser}/>
-            </div>
+            <div className="box box2"><ChartBox {...chartBoxUser}/></div>
             <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-            <div className="box box4">4</div>
+            <div className="box box4"><PieChartBox/></div>
             <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
             <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
-            <div className="box box7">7</div>
-            <div className="box box8">8</div>
-            <div className="box box9">9</div>
+            <div className="box box7"><BigChartBox/></div>
+            <div className="box box8"><BarChartBox {...barChartBoxRevenue}/></div>
+            <div className="box box9"><BarChartBox {...barChartBoxVisit}/></div>
         </div>
     )
 }
